46a3e041de81c67cfdcdef5772e590735af10e26
lim5max <44651051+lim5max@users.noreply.github.com>
2020-11-10 22:27:08 +0300

    add

--- js/valueslider.js
+++ js/valueslider.js
@@ -6,4 +6,34 @@ var rangeValue = function(){
   target.innerHTML = newValue;
 }
 
-slider_elem.addEventListener("input", rangeValue);
+slider_elem.addEventListener("input", rangeValue);var circle = document.querySelector('circle');
+var radius = circle.r.baseVal.value;
+var circumference = radius * 2 * Math.PI;
+
+circle.style.strokeDasharray = `${circumference} ${circumference}`;
+circle.style.strokeDashoffset = `${circumference}`;
+
+function setProgress(percent) {
+  const offset = circumference - percent / 100 * circumference;
+  circle.style.strokeDashoffset = offset;
+}
+
+const input = document.querySelector('#circle-container input');
+const button1 = document.querySelector('#circle-container #min');
+const button2 = document.querySelector('#circle-container #plus');
+setProgress(input.value);
+console.log(input.value)
+
+button1.addEventListener('click', function(e) {
+console.log(1)
+  if ( parseInt(input.value) < 101 && parseInt(input.value) > -1) {
+    setProgress(parseInt(input.value));
+  }  
+})
+button2.addEventListener('click', function(e) {
+console.log(1)
+  if ( parseInt(input.value) < 101 && parseInt(input.value) > -1) {
+    setProgress(parseInt(input.value));
+  }  
+})
+
